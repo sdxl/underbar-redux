698775389943c7e349f780fe6834e34229a2b5cf
MKS-students <mks.students@hackreactor.com>
2015-07-27 16:48:09 -0500

    Completed through defaults

--- src/underbar.js
+++ src/underbar.js
@@ -195,12 +195,26 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+    return !!_.reduce(collection, function(trueSoFar, value) {
+      return trueSoFar && !!iterator(value);
+    }, true);
+
+
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
+    return !_.every(collection, function(value) {
+      if (iterator(value)) {
+        return false;
+      } else {
+        return true;
+      }
+    });
   };
 
 
@@ -223,11 +237,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        obj[key] = arguments[i][key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+    for (var key in arguments[i]) {
+        if(obj[key] === undefined){
+        obj[key] = arguments[i][key];
+      }
+    }
+  }
+    return obj;
   };
 
 
@@ -271,6 +299,8 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+
+
   };
 
   // Delays a function for the given number of milliseconds, and then calls
